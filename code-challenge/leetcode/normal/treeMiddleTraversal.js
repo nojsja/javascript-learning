374d3c1909d744b8282b147a710b5dbe8fecbddb
NoJsJa <yangwei13@outlook.com>
2020-12-13 00:11:21 +0800

    update: tree inorderTraversal

--- code-challenge/leetcode/normal/treeMiddleTraversal.js
+++ code-challenge/leetcode/normal/treeMiddleTraversal.js
@@ -1,6 +1,6 @@
 /* -------------------------------------------------------
   description:
-  给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+  给定一个二叉树的前序遍历 ，返回它的 中序 遍历。
   输入：root = [1,null,2,3]
   输出：[1,3,2]
   tree:           1
@@ -48,10 +48,30 @@ var processor = function() {
     result.push(tree.value);
     treeMiddleTraversal(tree.right)
   };
+
+  // rootTree && treeMiddleTraversal(rootTree);
+  // return result;
+
+  /* 将树以中序遍历返回(方法2：使用迭代) */
+  var inorderTraversal = function(root) {
+    var stack = [];
+    var res = [];
   
-  rootTree && treeMiddleTraversal(rootTree);
+    while (root || stack.length) {
+      while (root && root.value) {
+        stack.push(root);
+        root = root.left;
+      }
   
-  return result;
+      root = stack.pop();
+      res.push(root.value);
+      root = root.right;
+    }
+  
+    return res;
+  };
+
+  return inorderTraversal(rootTree);
 }
 
 console.log(processor(root));
