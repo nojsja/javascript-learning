f73c32c3519ef6747ae7f8df39d5ac491ccdde25
NoJsJa <yangwei13@outlook.com>
2021-02-27 10:41:13 +0800

    update

--- sorting-algorithm/bubbleSort.js
+++ sorting-algorithm/bubbleSort.js
@@ -7,7 +7,7 @@ var origin = [32, 43, 1, 100, 10, 11, 50, 7, 8, 200, 2, 1000];
 
 function bubbleSort(origin) {
   var temp;
-  for (var i = 0; i < origin.length ; i++) {
+  for (var i = 0; i < origin.length - 1 ; i++) {
     for (var j = 0; j < origin.length - 1 - i; j++) {
       if (origin[j] > origin[j+1]) {
         temp = origin[j];
--- sorting-algorithm/quickSort.js
+++ sorting-algorithm/quickSort.js
@@ -3,12 +3,12 @@
 * @description: 快速排序
 */
 
-var origin = [32, 43, 1, 100, 10, 11, 50, 7, 8, 200, 2, 1000];
+var origin = [32, 43, 1, 100, 10, 11, 50, 7, 8, 200, 2, 1000, 8];
 
 function quickSort(origin) {
   if (!origin.length) return origin;
 
-  var left = [], right = [];
+  var left = [], right = [], middle = [];
   var flag = origin[(origin.length / 2) ^ 0];
 
   for (var i = 0; i < origin.length; i++) {
@@ -16,11 +16,13 @@ function quickSort(origin) {
       right.push(origin[i]);
     } else if(origin[i] < flag) {
       left.push(origin[i]);
+    } else {
+      middle.push(origin[i])
     }
   }
 
   return quickSort(left)
-    .concat(flag)
+    .concat(middle)
     .concat(quickSort(right));
 }
 
