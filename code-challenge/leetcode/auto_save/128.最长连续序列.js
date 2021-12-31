aba20095a13d84b5edc56dd51d615f9074cce9db
NoJsja <yangwei13@outlook.com>
2022-01-01 00:25:34 +0800

    update: leetcode

---
+++ code-challenge/leetcode/auto_save/128.最长连续序列.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=128 lang=javascript
+ *
+ * [128] 最长连续序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+
+};
+// @lc code=end
+
---
+++ code-challenge/leetcode/auto_save/704.二分查找.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+
+};
+// @lc code=end
+
