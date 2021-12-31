0af963081bd67d557ca02816d9c40c70177957ba
NoJsja <yangwei13@outlook.com>
2022-01-01 00:30:21 +0800

    update

--- code-challenge/leetcode/auto_save/704.二分查找.js
+++ code-challenge/leetcode/auto_save/704.二分查找.js
@@ -11,7 +11,19 @@
  * @return {number}
  */
 var search = function(nums, target) {
+  var left = 0, right = nums.length - 1, mid = Math.floor((left + right) / 2);
 
+  while (mid !== right && mid !== left) {
+    if (nums[mid] == target) return mid;
+    if (nums[mid] < target) left = mid;
+    if (nums[mid] > target) right = mid;
+    mid = Math.floor((left + right) / 2);
+  }
+
+  if (nums[left] === target) return left;
+  if (nums[right] === target) return right;
+
+  return -1;
 };
 // @lc code=end
 
