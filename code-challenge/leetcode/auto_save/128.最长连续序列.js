b81bd8a93ce8e9c5eb4280597f51e70505ff902e
nojsja <yangwei13@outlook.com>
2022-01-02 01:11:49 +0800

    update: leetcode

--- code-challenge/leetcode/auto_save/128.最长连续序列.js
+++ code-challenge/leetcode/auto_save/128.最长连续序列.js
@@ -10,7 +10,22 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+  var record = new Set(nums);
+  var max = nums.length ? 1 : 0;
 
+  for (var i = 0; i < nums.length; i++) {
+    if (!record.has(nums[i] - 1)) {
+      var cur = nums[i] + 1;
+      var tmp = 1;
+      while(record.has(cur)) {
+        tmp += 1;
+        max = Math.max(max, tmp);
+        cur += 1;
+      }
+    }
+  }
+
+  return max;
 };
 // @lc code=end
 
